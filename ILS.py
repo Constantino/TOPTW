13ffda34e13c39ee925c2111ed8fd95696654022
Constantino Mora <constantinomora@gmail.com>
2016-05-08 23:39:15 -0500

    added getTour()

--- ILS.py
+++ ILS.py
@@ -1,12 +1,53 @@
 class ILS:
 
 	def shake(self,Locations, R,S):
-		#print "in 1"
-		for l in range(S,R+1):
+		
+		for l in range(S,S+R+1):
 			i = 0 if l >= len(Locations) else l
 			print "removing location: ",Locations[i].id_location
 			Locations.remove(Locations[i])
 
 		return Locations
+
+	def execute():
+
+		S = 1
+		R = 1
+
+		NoImprovementCounter = 0
+
+		BestFound = {"ratio": 0, "tour":0}
+
+		SmallestTourSize = 0
+
+		while NoImprovementCounter < 150:
+			
+			NewTour = 0; #Get tour
+			if BestFound['tour'] < NewTour:
+				
+				#Assign new tour as local optimum
+				BestFound['tour'] = NewTour
+
+				R = 1
+				NoImprovementCounter = 0
+
+			else:
+				NoImprovementCounter += 1
+
+			tour = self.shake(BestFound['tour'], R, S)
+
+			S = S + R
+			R = R + 1
+
+			if S >= SmallestTourSize:
+				S = S - SmallestTourSize
+
+			if R == (1.0*n/(3*n)):
+				R = 1
+
+		return BestFound
+
+	
+
 
 
--- main.py
+++ main.py
@@ -23,14 +23,57 @@ def print_locations(Locations):
 		print " "
 		
 
+def getTourRatio(Locations):
+	ratio = 0
+	for e in Locations:
+		ratio += e.ratio
+
+	return ratio
+
+
+def getTour(Locations, times, start, end):
+	#Initialize all locations as if each of them where inserted between start and end of tour
+	Locations = InsertionStep.update_locations(Locations,times,start,end)
+
+	print_locations(Locations)
+	print "........"
+	selected_locations = []
+
+
+	#Add start location to the tour
+	selected_locations.append(Locations[0])
+	selected_locations.append(Locations[len(Locations)-1])
+
+	req_t = 1
+
+	while len(Locations) > 2:
+		print "***---***"
+		potential_inserts,local_information = InsertionStep.simulate_insertion(Locations, selected_locations, times)
+		print "potential_inserts:"
+		print_locations( potential_inserts )
+		print "</potential_inserts"
+
+		selected_one =  InsertionStep.select_potential_location(potential_inserts)
+		print "selected one : ",selected_one.id_location
+		before = local_information[selected_one.id_location]
+		print "insert after : ",before
+		selected_locations.insert(before+1,selected_one)
+		Locations.remove(selected_one)
+		print "***---***"
+		selected_locations = InsertionStep.update_stuff(selected_locations,times,start)
+
+	print_locations(selected_locations)
+
+	return selected_locations
+
 start = 8 #hours
 end = 23 #hours
 n = 5 #no. elements
 instance = random_instance()
 
-Locations = instance.generate(n,start,end)
+Locations = instance.generate(n,start,end,5)
 
-times = instance.generate_times(n)
+times = instance.generate_times(n,5)
 
 print_locations(Locations)
 
@@ -40,38 +83,12 @@ for e in times:
 
 InsertionStep = insertion_step()
 
-#Initialize all locations as if each of them where inserted between start and end of tour
-Locations = InsertionStep.update_locations(Locations,times,start,end)
 
-print_locations(Locations)
-print "........"
-selected_locations = []
+#ILS
+
+tour = getTour(Locations, times, start, end)
+
+print "Tour ratio: ", getTourRatio(tour)
 
 
-#Add start location to the tour
-selected_locations.append(Locations[0])
-#xprint "append: ",Locations[len(Locations)-1].id_location
-selected_locations.append(Locations[len(Locations)-1])
-#<exp>
 
-req_t = 1
-
-while len(Locations) > 2:
-	print "***---***"
-	potential_inserts,local_information = InsertionStep.simulate_insertion(Locations, selected_locations, times)
-	print "potential_inserts:"
-	print_locations( potential_inserts )
-	print "</potential_inserts"
-
-	selected_one =  InsertionStep.select_potential_location(potential_inserts)
-	print "selected one : ",selected_one.id_location
-	before = local_information[selected_one.id_location]
-	print "insert after : ",before
-	selected_locations.insert(before+1,selected_one)
-	Locations.remove(selected_one)
-	print "***---***"
-
-	selected_locations = InsertionStep.update_stuff(selected_locations,times,start)
-	
-
-print_locations(selected_locations)
